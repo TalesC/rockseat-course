c5ce84c88fe13026b266f3b6973031cad904d595
TalesC <talescarvalhopereira@gmail.com>
2020-07-19 16:46:06 -0300

    class about classes

--- ES6/main.js
+++ ES6/main.js
@@ -1,9 +1,30 @@
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outro(){
-
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends List{
+    constructor() {
+        super();
+
+        this.usuatio = 'Diego';
+    }
+
+    mostrarUsuario() {
+        console.log(this.usuatio);
+    }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+};
+
+MinhaLista.mostrarUsuario();
